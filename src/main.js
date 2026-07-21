29032d67c7fd4b2c0ff84fab247e5a4b18dc702d
cory56626-art <cory56626@gmail.com>
2026-07-21 16:54:42 -0400

    Refresh app module after selector fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { el } from './core/util.js';
 import { loadSettings, hasSave, loadSave, setState, save as persist } from './core/store.js';
 import { createNewCareer } from './game/newgame.js';
-import { app } from './ui/app.js?build=9e165c4';
+import { app } from './ui/app.js?build=9e165c4b';
 import { runPackOpening } from './ui/pack-open.js';
 
 function boot() {
