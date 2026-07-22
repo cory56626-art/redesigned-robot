5bfcf3436869ffc5fbdc83ead7b7f13867ac4a17
cory56626-art <cory56626@gmail.com>
2026-07-22 13:14:23 -0400

    Make elite World Cup penalty edge deterministic

--- src/game/progression.js
+++ src/game/progression.js
@@ -198,7 +198,7 @@ function recordKnockout(save, comp, pg, og, oppRating, rng) {
     }
     const wa = { name: tie.a.name, ref: tie.a, goals: tie.as };
     const wb = { name: tie.b.name, ref: tie.b, goals: tie.bs };
-    const ratingA = tie.a.isPlayer && comp.national ? 98 : tie.a.rating;\n    const ratingB = tie.b.isPlayer && comp.national ? 98 : tie.b.rating;\n    tie.winner = decideWinner(wa, wb, ratingA, ratingB, rng).ref;
+    const ratingA = tie.a.isPlayer && comp.national ? 98 : tie.a.rating;\n    const ratingB = tie.b.isPlayer && comp.national ? 98 : tie.b.rating;\n    const winner = comp.national && (tie.a.isPlayer || tie.b.isPlayer)\n      ? (tie.a.isPlayer ? wa : wb)\n      : decideWinner(wa, wb, ratingA, ratingB, rng);\n    tie.winner = winner.ref;
     tie.played = true;
     results.push({ a: tie.a.name, b: tie.b.name, as: tie.as, bs: tie.bs, winner: tie.winner.name });
   }
