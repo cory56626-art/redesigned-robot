84fa735858733de294e46651063cd9d6265ba3fb
cory56626-art <cory56626@gmail.com>
2026-07-21 21:12:01 -0400

    Bust app cache after shot throttle

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import { el } from './core/util.js';
 import { loadSettings, hasSave, loadSave, setState, save as persist } from './core/store.js';
 import { createNewCareer } from './game/newgame.js';
-import { app } from './ui/app.js?build=344cb55e';
+import { app } from './ui/app.js?build=5832ec97';
 import { runPackOpening } from './ui/pack-open.js';
 
 function boot() {
